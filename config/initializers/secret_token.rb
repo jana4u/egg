5a89d9a4880e23771f826b58c10b5188419d09e6
jana4u <jana4u@seznam.cz>
2011-08-19 14:59:36 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Egg::Application.config.secret_token = 'ccb2ef277968c2190327180cacb06269e231f07631fd27c302dbc0c80151f06d7332b6707f90993db9d6b6d363defa35b262465c752d6df85f77a85c90707b3c'
